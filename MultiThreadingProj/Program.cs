54965006fd244bf1e48aa8197ab8e0bb248d68fc
Vasiliy Cherniatyn <v.v.4ern9tin@gmail.com>
2016-04-14 00:30:00 +0300

    Add new functionality

--- MultiThreadingProj/Program.cs
+++ MultiThreadingProj/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace MultiThreadingProj
     class Program
     {
         static Queue<string> _queue = new Queue<string>();
-       
+        static object _check = false;
         static void Main(string[] args)
         {
             Thread thread = new Thread(SetMessage);
@@ -24,9 +25,20 @@ namespace MultiThreadingProj
 
         static void SetMessage()
         {
+            string read = "";
             while (true)
             {
-                _queue.Enqueue(Console.ReadLine());
+                read = Console.ReadLine();
+
+                if (read.ToUpper().Equals("EXIT"))
+                {
+                    lock(_check)
+                    {
+                        _check = true;
+                        Thread.CurrentThread.Abort();
+                    }
+                }
+                _queue.Enqueue(read);
             }
         }
 
@@ -34,16 +46,51 @@ namespace MultiThreadingProj
         {
             while (true)
             {
-                Console.WriteLine($"\n-----------Sending {_queue.Count} message !-------------");
-                while (_queue.Count > 0)
-                {
-                    _queue.Dequeue();
-                }
                 Thread.Sleep(5000);
+                int count = _queue.Count;
+                int _count;
 
+                if (count == 0)
+                {
+                    _count = 0;
+                }
+                else if (count <= 3)
+                {
+                    _count = 1;
+                }
+                else
+                {
+                    if (count % 3 >= 1)
+                    {
+                        _count = count / 3 + 1;
+                    }
+                    else
+                    {
+                        _count = count / 3;
+                    }
+                }
+                int _count2 = _count;
+                //_count = count > 3 ? (count / 3 + count % 3 >= 1 ? 1 : 0) : 1;
+                while (_count-- > 0)
+                {
+                    new Thread(SendThreeMessage).Start(3);
+                }
+                lock (_check)
+                {
+                    if (_check.Equals(true))
+                        Thread.CurrentThread.Abort();
+                }
+                Console.WriteLine($"\n------Sending {count} message !------{_count2} thread started------");
             }
         }
-            
-
+        static void SendThreeMessage(Object stateInfo)
+        {
+            int _count = (int)stateInfo;
+            while (_queue.Count > 0 && _count-- > 0)
+            {
+                _queue.Dequeue();
+            }
+            Thread.CurrentThread.Abort();
+        }
     }
 }
